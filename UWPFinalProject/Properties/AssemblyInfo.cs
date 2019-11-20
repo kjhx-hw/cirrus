d626318e2bf239b70a15be5bdf9f4e18d8f5a91c
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2019-11-20 16:25:07 -0600

    Create basic NavigationView structure

--- UWPFinalProject/Properties/AssemblyInfo.cs
+++ UWPFinalProject/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UWPFinalProject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cirrus")]
+[assembly: AssemblyDescription("A simple UWP application for interacting with SoundCloud.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("T7")]
 [assembly: AssemblyProduct("UWPFinalProject")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
