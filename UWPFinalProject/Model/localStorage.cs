b36a9bb2af1863fa03a92585e0af89efd52f594e
FairyTamerRyles <54481765+FairyTamerRyles@users.noreply.github.com>
2019-11-25 17:18:01 -0600

    LocalStorage Methods
    added methods for adding and removing items from the favorites lists, as well as serializing and deserializing them to be put into local storage.

--- UWPFinalProject/Model/localStorage.cs
+++ UWPFinalProject/Model/localStorage.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using SoundCloud.Api;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace UWPFinalProject.Model
-{
-    class localStorage
-    {
+namespace UWPFinalProject.Model {
+    class LocalStorage {
+        private string storedFavorites;
+        private List<int> favoritesList;
+
+        public string StoredFavorites {
+            get {
+                return storedFavorites;
+            }
+            set {
+                storedFavorites = value;
+            }
+        }
+        public LocalStorage() {
+
+        }
+        public void addItem(string newItem) {
+            int newId = Convert.ToInt32(newItem);
+            if(favoritesList.Contains(newId)) {
+                //alert user that item is already in favorites list
+            } else {
+                favoritesList.Add(newId);
+            }
+        }
+
+        public void deleteItem(string itemToDelete)
+        {
+            int IdToDelete = Convert.ToInt32(itemToDelete);
+            if (favoritesList.Contains(IdToDelete)) {
+                favoritesList.Remove(IdToDelete);
+            }
+        }
+        public void serializeFavorites() {
+            string json = JsonConvert.SerializeObject(favoritesList);
+            storedFavorites = json;
+        }
+        public void deserializeFavorites() {
+            favoritesList = JsonConvert.DeserializeObject<List<int>>(storedFavorites);
+        }
     }
 }
