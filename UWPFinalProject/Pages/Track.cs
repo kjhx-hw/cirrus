35e1e4bf424c528852319ded3451eb1bfb878fd2
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2019-11-29 15:30:35 -0600

    Data Binding test

---
+++ UWPFinalProject/Pages/Track.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPFinalProject.Pages {
+    public class Track {
+        public string TrackName { get; set; }
+        public string ArtistName { get; set; }
+        public int TrackId { get; set; }
+        public string TrackArtUrl { get; set; }
+
+        public Track() {
+            // Defaults
+            this.TrackName = "Hobnotropic";
+            this.ArtistName = "matas";
+            this.TrackId = 49931;
+            this.TrackArtUrl = "https://i1.sndcdn.com/artworks-000000103093-941e7e-large.jpg";
+        }
+    }
+}
---
+++ UWPFinalProject/Pages/TrackViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPFinalProject.Pages {
+    public class TrackViewModel {
+        private ObservableCollection<Track> tracks = new ObservableCollection<Track>();
+        public ObservableCollection<Track> Tracks { get { return this.tracks; } }
+
+        public TrackViewModel() {
+            this.tracks.Add(new Track());
+            this.tracks.Add(new Track());
+            this.tracks.Add(new Track());
+            this.tracks.Add(new Track());
+            this.tracks.Add(new Track());
+            this.tracks.Add(new Track());
+        }
+    }
+}
